72acec83bd757e8707e8113311cfdef86794340e
Abbas Fakih <abbassfakih54321@outlook.com>
2025-06-03 19:20:55 +0300

    my work

--- controllers/shelterController.js
+++ controllers/shelterController.js
@@ -92,3 +92,41 @@ exports.deleteShelter = async (req, res) => {
         res.status(500).json({ message: err.message });
     }
 };
+
+exports.bookShelter = async (req, res) => {
+    try {
+        const { shelter_id, user_id } = req.body;
+
+        const user = await User.findById(user_id);
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        if (user.userType !== 'Shelter_requester') {
+            return res.status(403).json({ message: "Only shelter requesters can book shelters" });
+        }
+
+        const shelter = await Shelter.findOne({ shelter_id: shelter_id });
+        if (!shelter) {
+            return res.status(404).json({ message: "Shelter not found" });
+        }
+
+        if (shelter.current_occupancy >= shelter.capacity) {
+            return res.status(400).json({ message: "Shelter is full, no available space" });
+        }
+
+        const updatedShelter = await Shelter.findOneAndUpdate(
+            { shelter_id: shelter_id },
+            { $inc: { current_occupancy: 1 } },
+            { new: true }
+        );
+
+        return res.status(200).json({ 
+            data: updatedShelter, 
+            message: `Shelter booked successfully. Available spots: ${updatedShelter.capacity - updatedShelter.current_occupancy}` 
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: err.message });
+    }
+};
